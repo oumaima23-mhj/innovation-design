80934759d9d5dfcefb0543cefa393b88e5862350
Ali Mahjoub <alimahjoub047@gmail.com>
2023-12-04 23:46:12 +0100

    les demande mriglin

---
+++ admin/demande carte.php
@@ -0,0 +1,104 @@
+<?php include "nav.php";
+?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Tables Carte Visite</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Tables</li>
+                        </ol>
+                        
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                DataTable
+                            </div>
+                            <div class="card-body">
+                            <?php
+// Connexion à la base de données - à remplacer avec vos propres informations
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "innovation_design";
+
+// Connexion à la base de données
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Requête pour récupérer les données de la table demande_carte_visite
+$sql = "SELECT * FROM demande_carte_visite";
+$result = $conn->query($sql);
+
+// Afficher les données dans le format de table HTML
+if ($result->num_rows > 0) {
+    echo '<table id="datatablesSimple">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>id</th>';
+    echo '<th>nom_entreprise</th>';
+    echo '<th>nom_personne_contact</th>';
+    echo '<th>email</th>';
+    echo '<th>telephone</th>';
+    echo '<th>adresse</th>';
+    echo '<th>exemple_1</th>';
+    echo '<th>exemple_2</th>';
+    echo '<th>notes_supplementaires</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $row["id"] . '</td>';
+        echo '<td>' . $row["nom_entreprise"] . '</td>';
+        echo '<td>' . $row["nom_personne_contact"] . '</td>';
+        echo '<td>' . $row["email"] . '</td>';
+        echo '<td>' . $row["telephone"] . '</td>';
+        echo '<td>' . $row["adresse"] . '</td>';
+        echo '<td>' . $row["exemple_1"] . '</td>';
+        echo '<td>' . $row["exemple_2"] . '</td>';
+        echo '<td>' . $row["notes_supplementaires"] . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</tbody>';
+    echo '</table>';
+} else {
+    echo "0 results";
+}
+
+// Fermer la connexion à la base de données
+$conn->close();
+?>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
